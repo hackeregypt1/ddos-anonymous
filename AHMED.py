f8762374d4a51ce5bee0b37dfd30365b53344672
hackeregypt1 <73580235+hackeregypt1@users.noreply.github.com>
2020-10-31 17:26:43 +0200

    Update and rename README.md to AHMED.py

---
+++ AHMED.py
@@ -0,0 +1,14 @@
+# ddos-anonymous
+
+#to install tool
+
+#comand
+
+# apt install git && apt install python -y
+
+#git clone https://github.com/hackeregypt1/ddos-anonymous/
+
+
+#cd ddos-anonymous
+
+# 
